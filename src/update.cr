f09b748ae9c698ce50b53aa0dbca4b00b03764c9
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-09-18 17:58:27 -0600

    Document some of the script and only use config file if it exists

--- src/update.cr
+++ src/update.cr
@@ -1,24 +1,34 @@
 require "./update/provider"
 
+# Embed a copy of the default Providers in the binary.
 yaml = {{ `cat #{__DIR__}/providers.yml`.stringify }}
 
+# Create an Array of Providers from the default data.
 providers = Array(Update::Provider).from_yaml yaml
 
-user_yaml = File.read File.join(`printf $HOME`, ".update.yml")
-user_providers = Array(Update::Provider).from_yaml user_yaml
+# If the user has a configuration file add its providers to the array.
+if (File.exists?(File.join(`printf $HOME`, ".update.yml")))
+  user_yaml_file = File.join(`printf $HOME`, ".update.yml")
 
-user_providers.each do |user_provider|
-  provider = providers.find &.==(user_provider)
-  next providers << user_provider unless provider
-  provider.merge! user_provider
+  user_yaml = File.read user_yaml_file
+  user_providers = Array(Update::Provider).from_yaml user_yaml
+
+  user_providers.each do |user_provider|
+    provider = providers.find &.==(user_provider)
+    next providers << user_provider unless provider
+    provider.merge! user_provider
+  end
 end
 
+# Promote providers than can actually be run.
 providers.select! &.updatable?
 
+# Reject any providers that duplicate existing ones. (Use case: pacman)
 providers.reject! do |equal_provider|
   orig_provider = providers.find &.replaceable?(equal_provider)
   next false unless orig_provider
   orig_provider.merge! equal_provider
 end
 
+# Run all the providers.
 providers.each &.update
